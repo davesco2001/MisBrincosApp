1353013490023fba1c2f053c97cc53e738ce2e74
Sofia Nuñez <lauranuri@unisabana.edu.co>
2021-10-16 23:26:37 -0500

    settings in the physical part

--- app/src/main/java/com/example/misbrincosapp/EditSessionsActivity.java
+++ app/src/main/java/com/example/misbrincosapp/EditSessionsActivity.java
@@ -1,14 +1,25 @@
 package com.example.misbrincosapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class EditSessionsActivity extends AppCompatActivity {
+
+    FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_edit_sessions);
+        currentUser = mAuth.getCurrentUser();
     }
 }
