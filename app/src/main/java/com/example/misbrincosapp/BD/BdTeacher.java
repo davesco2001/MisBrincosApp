24befc11ea01b01a10d93ce0a2287a63dfbc185c
davesco2001 <88469090+davidesco201@users.noreply.github.com>
2021-10-30 11:01:52 -0500

    Bd Teacher

--- app/src/main/java/com/example/misbrincosapp/BD/BdTeacher.java
+++ app/src/main/java/com/example/misbrincosapp/BD/BdTeacher.java
@@ -77,4 +77,144 @@ public class BdTeacher {
         }
         return arrayList;
     }
+    public ArrayList<String> searchCc() {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        String sql = "SELECT Cc FROM Profesor ORDER BY Cc" ;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                arrayList.add(resultSet.getString("Cc"));
+            }
+
+        } catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            // + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+        return arrayList;
+    }
+    public ArrayList<String> searchName() {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        String sql = "SELECT Nombre FROM Profesor ORDER BY Cc" ;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                arrayList.add(resultSet.getString("Nombre"));
+            }
+
+        } catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            // + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+        return arrayList;
+    }
+    public ArrayList<String> searchEmail() {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        String sql = "SELECT Correo FROM Profesor ORDER BY Cc" ;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                arrayList.add(resultSet.getString("Correo"));
+            }
+
+        } catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            // + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+        return arrayList;
+    }
+    public ArrayList<String> searchTeacherCc(String cc) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        String sql = "SELECT Cc FROM Profesor WHERE Cc='"+cc+"'";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                arrayList.add(resultSet.getString("Cc"));
+            }
+
+        } catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            //        + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+        return arrayList;
+    }
+    public ArrayList<String> searchTeacherNameWith(String cc) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        String sql = "SELECT Nombre FROM Profesor WHERE Cc='"+cc+"'";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                arrayList.add(resultSet.getString("Nombre"));
+            }
+
+        } catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            //        + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+        return arrayList;
+    }
+    public ArrayList<String> searchTeacherEmail(String cc) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        String sql = "SELECT Correo FROM Profesor WHERE Cc='"+cc+"'";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                arrayList.add(resultSet.getString("Correo"));
+            }
+
+        } catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            //        + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+        return arrayList;
+    }
+    public void addTeacher(String cc,String name, String email){
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Profesor VALUES ('"+cc+"' ,'"+name+"','"+email+"')")) {
+            preparedStatement.executeUpdate();
+        }catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            //        + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+
+    }
+    public void updateTeacher(String cc, String email) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE Profesor SET Correo='"+email+"' WHERE Cc='"+cc+"'")) {
+            preparedStatement.executeUpdate();
+        }catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            //        + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+    }
+    public void deleteTeacher(String cc){
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Profesor WHERE Cc='"+cc+"'")) {
+            preparedStatement.executeUpdate();
+        }catch (SQLException sqlException) {
+            //toast.makeText(createLessonsActivity,"Error en la ejecución:"
+            //        + sqlException.getErrorCode() + " " + sqlException.getMessage() , Toast.LENGTH_SHORT).show();
+            System.out.println("Error en la ejecución:"
+                    + sqlException.getErrorCode() + " " + sqlException.getMessage());
+        }
+
+    }
 }
